0e820c6f15aa0e8bc67e1b8c959f959f849beeef
Tyler Huynh <tylerhuynh100@gmail.com>
2016-09-11 12:19:31 -0700

    Update tournament_test.py

--- tournament_test.py
+++ tournament_test.py
@@ -2,9 +2,7 @@
 #
 # Test cases for tournament.py
 # These tests are not exhaustive, but they should cover the majority of cases.
-#
-# If you do add any of the extra credit options, be sure to add/modify these test cases
-# as appropriate to account for your module's added functionality.
+
 
 from tournament import *
 
